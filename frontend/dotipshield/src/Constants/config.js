106ee95c7b1eecadd65a2a4c2f5dbee91df9f79c
Dawe000 <dawid.pisarczyk@gmail.com>
2024-07-21 10:21:32 +0100

    new contracts addresses and abis

--- frontend/dotipshield/src/Constants/config.js
+++ frontend/dotipshield/src/Constants/config.js
@@ -1,120 +1,138 @@
-export const registrationAddress = '0x79c83BB052FFea03e27233762784d2030Ae219FE';
-export const transferAddress =     '0xA68e9843aae7143f0e079A0630295571E2768b1A';
+export const registrationAddress = '0xF6ED0DAA07eE6805507F6Ed12222c90fD59F0548';
+export const transferAddress =     '0x79274af1F0aaAB3921dea8a51d9864E2B8a2A22C';
 //export const verificationAddress = ;
 
 export const registrationABI = [
     {
-        "anonymous": false,
-        "inputs": [
-          {
-            "indexed": true,
-            "internalType": "address",
-            "name": "owner",
-            "type": "address"
-          },
-          {
-            "indexed": false,
-            "internalType": "string",
-            "name": "ipfsHash",
-            "type": "string"
-          }
-        ],
-        "name": "IPRegistered",
-        "type": "event"
-      },
-      {
-        "inputs": [],
-        "name": "getAllIPs",
-        "outputs": [
-          {
-            "components": [
-              {
-                "internalType": "address",
-                "name": "owner",
-                "type": "address"
-              },
-              {
-                "internalType": "uint256",
-                "name": "timestamp",
-                "type": "uint256"
-              },
-              {
-                "internalType": "string",
-                "name": "ipfsHash",
-                "type": "string"
-              }
-            ],
-            "internalType": "struct IPRegistration.IP[]",
-            "name": "",
-            "type": "tuple[]"
-          }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "inputs": [
-          {
-            "internalType": "string",
-            "name": "ipfsHash",
-            "type": "string"
-          }
-        ],
-        "name": "getIP",
-        "outputs": [
-          {
-            "components": [
-              {
-                "internalType": "address",
-                "name": "owner",
-                "type": "address"
-              },
-              {
-                "internalType": "uint256",
-                "name": "timestamp",
-                "type": "uint256"
-              },
-              {
-                "internalType": "string",
-                "name": "ipfsHash",
-                "type": "string"
-              }
-            ],
-            "internalType": "struct IPRegistration.IP",
-            "name": "",
-            "type": "tuple"
-          }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-      },
-      {
-        "inputs": [
-          {
-            "internalType": "string",
-            "name": "ipfsHash",
-            "type": "string"
-          }
-        ],
-        "name": "registerIP",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-      },
-      {
-        "inputs": [
-          {
-            "internalType": "address",
-            "name": "newTransferContractAddress",
-            "type": "address"
-          }
-        ],
-        "name": "setTransferContract",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-      }
-];
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "owner",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "string",
+          "name": "ipfsHash",
+          "type": "string"
+        }
+      ],
+      "name": "IPRegistered",
+      "type": "event"
+    },
+    {
+      "inputs": [],
+      "name": "getAllIPs",
+      "outputs": [
+        {
+          "components": [
+            {
+              "internalType": "address",
+              "name": "owner",
+              "type": "address"
+            },
+            {
+              "internalType": "uint256",
+              "name": "timestamp",
+              "type": "uint256"
+            },
+            {
+              "internalType": "string",
+              "name": "ipfsHash",
+              "type": "string"
+            }
+          ],
+          "internalType": "struct IPRegistration.IP[]",
+          "name": "",
+          "type": "tuple[]"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "ipfsHash",
+          "type": "string"
+        }
+      ],
+      "name": "getIP",
+      "outputs": [
+        {
+          "components": [
+            {
+              "internalType": "address",
+              "name": "owner",
+              "type": "address"
+            },
+            {
+              "internalType": "uint256",
+              "name": "timestamp",
+              "type": "uint256"
+            },
+            {
+              "internalType": "string",
+              "name": "ipfsHash",
+              "type": "string"
+            }
+          ],
+          "internalType": "struct IPRegistration.IP",
+          "name": "",
+          "type": "tuple"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "ipfsHash",
+          "type": "string"
+        }
+      ],
+      "name": "registerIP",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "ipfsHash",
+          "type": "string"
+        },
+        {
+          "internalType": "address",
+          "name": "newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "registerIP",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "newTransferContractAddress",
+          "type": "address"
+        }
+      ],
+      "name": "setTransferContract",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ];
 
 export const TransferABI = [
     {
