b00ed53441a2c63df46671343c6410c5e3f57718
Dawe000 <dawid.pisarczyk@gmail.com>
2024-07-21 10:59:00 +0100

    chnages to transfer

--- frontend/dotipshield/src/components/transferForm.tsx
+++ frontend/dotipshield/src/components/transferForm.tsx
@@ -60,6 +60,7 @@ const TransferForm: React.FC = () => {
           type="submit"
           className="px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700"
         >
+          Submit
         </button>
       </form>
     </div>
